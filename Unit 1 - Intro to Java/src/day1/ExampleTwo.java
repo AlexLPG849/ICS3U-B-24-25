88ade9fbc1e13c74beea0c2a659ff3e9fd3d8fb8
Alex Lee <allee@bayviewglen.ca>
2024-09-09 10:32:51 -0400

    Initial Commit

---
+++ Unit 1 - Intro to Java/src/day1/ExampleTwo.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("This is great.");
+        System.out.println("This is nice.");
+        System.out.print("This is great.");
+
+
+    }
+}
